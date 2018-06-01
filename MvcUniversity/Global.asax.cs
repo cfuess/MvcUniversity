371b13b56a9de44144599b8b96fa11e31647ad7c
Chris Fuess <cfuess@LIVE.COM>
2018-06-01 00:14:19 -0700

    initial commit

---
+++ MvcUniversity/Global.asax.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using UniversityData;
+
+namespace MvcUniversity
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            // Database.SetInitializer<SchoolContext>(null);  // Leave the db alone
+            // Database.SetInitializer<SchoolContext>(new DropCreateDatabaseIfModelChanges<SchoolContext>());  // Recreate if model changes 
+            Database.SetInitializer(new SchoolInitializer());  // Recreate if model changes and Seed the db
+
+            using (var context = new SchoolContext())
+            {
+                context.Database.Initialize(true);
+            }
+
+
+        }
+    }
+}
